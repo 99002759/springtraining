25600a9e2ecac093d37e34b6d9e6afc72d3872a8
99002759 <72536632+99002759@users.noreply.github.com>
2020-10-27 18:13:19 +0530

    Add files via upload

---
+++ HotelApp/src/com/hotelapp/client/User.java
@@ -0,0 +1,34 @@
+package com.hotelapp.client;
+
+ 
+
+import java.util.List;
+
+ 
+
+import com.hotelapp.model.Hotel;
+import com.hotelapp.service.HotelService;
+import com.hotelapp.service.HotelServiceImpl;
+
+ 
+
+public class User {
+
+ 
+
+    public static void main(String[] args) {
+        HotelService HotelService=new HotelServiceImpl();
+        HotelService.getAllHotels();
+        List<Hotel> hotelList = HotelService.getAllHotels();
+        for(Hotel hotel:hotelList)
+            System.out.println(hotel);
+        // TODO Auto-generated method stub
+
+ 
+
+    }
+
+ 
+
+}
+ 
---
+++ HotelApp/src/com/hotelapp/service/HotelServiceImpl.java
@@ -0,0 +1,53 @@
+package com.hotelapp.service;
+
+ 
+
+import java.util.List;
+
+ 
+
+import com.hotelapp.dao.HotelDao;
+import com.hotelapp.dao.HotelDaoImpl;
+import com.hotelapp.model.Hotel;
+
+ 
+
+public class HotelServiceImpl implements HotelService {
+    HotelDao hotelDao=new HotelDaoImpl();
+
+ 
+
+    @Override
+    public List<Hotel> getAllHotels() {
+        // TODO Auto-generated method stub
+        return hotelDao.getAllHotels();
+    }
+
+ 
+
+    @Override
+    public Hotel getById(int id) {
+        // TODO Auto-generated method stub
+        return hotelDao.getById(id);
+    }
+
+ 
+
+    @Override
+    public List<Hotel> getByCity(String city) {
+        // TODO Auto-generated method stub
+        return hotelDao.getAllHotels();
+    }
+
+ 
+
+    @Override
+    public List<Hotel> getByCuisine(String cuisine) {
+        // TODO Auto-generated method stub
+        return hotelDao.getAllHotels();
+    }
+
+ 
+
+}
+ 
